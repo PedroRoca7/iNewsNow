d7d4c76b753a3b553fcbc3ba2c0ae920f571a7f1
Pedro Henrique Roca Moreira <pedrohvalles7@gmail.com>
2024-01-31 10:58:37 -0300

    Initial Commit

---
+++ INewsNow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  INewsNow
+//
+//  Created by Pedro Henrique on 31/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
