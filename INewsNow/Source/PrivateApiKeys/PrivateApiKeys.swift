407561de3ddb3542988dbc66cea90a6fb826d6b0
Pedro Henrique Roca Moreira <pedrohvalles7@gmail.com>
2024-04-11 16:12:19 -0300

    Please enter the commit message for your changes. Lines starting

--- INewsNow/Source/PrivateApiKeys/PrivateApiKeys.swift
+++ INewsNow/Source/PrivateApiKeys/PrivateApiKeys.swift
@@ -9,5 +9,5 @@ import Foundation
 
 struct PrivateApiKeys {
     static let newsBrazilApiKey = "pub_38681ecae91e7da84f59d75ee44ef2f63a0cb"
-    static let newsNewYorkTimeApiKey = "pub_38681ecae91e7da84f59d75ee44ef2f63a0cb"
+    static let newsNewYorkTimeApiKey = "N8U4kVw1Icdw3YiGFhThZqIGTAF9PJ7l"
 }
